d631c0c5337edc0ab8eb0ca39f0cb95bd7751811
Francois Lambert <francoisobob@gmail.com>
2024-04-29 13:30:11 -0400

    Initial Commit

---
+++ Monthly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Monthly
+//
+//  Created by Francois Lambert on 4/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
